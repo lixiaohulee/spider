f94af9e0a1c2116f5ede6f3be797f939ce63a857
lixiaohu <lixiaohu_neuq@163.com>
2019-02-02 09:41:24 +0800

    load img

--- app.js
+++ app.js
@@ -19,15 +19,15 @@ const headers = {
     "Accept-Language": "zh-CN,zh;q=0.9",
     "Cache-Control": "max-age=0",
     "Connection": "keep-alive",
-    "Cookie": "imooc_uuid=da540210-bba5-43cd-ae18-20d417a3e000; imooc_isnew_ct=1545119654; zg_did=%7B%22did%22%3A%20%22167c04ee69e0-0b6c173be5e1c-35657600-1fa400-167c04ee69f453%22%7D; imooc_isnew=2; loginstate=1; apsid=EyNGE4ZjdlMWU1ZTA3ZjA4ZDdlNGExMmZjNTkyNDcAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAANDk4Mzc3OQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADExM2FhNWRmNTVjYWI4MjNiMDFkNjFmYTI4ZWFlMGYwB%2BA%2BXAfgPlw%3DOW; IMCDNS=0; zg_f375fe2f71e542a4b890d9a620f9fb32=%7B%22sid%22%3A%201549011432361%2C%22updated%22%3A%201549011432383%2C%22info%22%3A%201549011432367%2C%22superProperty%22%3A%20%22%7B%5C%22%E5%BA%94%E7%94%A8%E5%90%8D%E7%A7%B0%5C%22%3A%20%5C%22%E6%85%95%E8%AF%BE%E7%BD%91%E6%95%B0%E6%8D%AE%E7%BB%9F%E8%AE%A1%5C%22%2C%5C%22Platform%5C%22%3A%20%5C%22web%5C%22%7D%22%2C%22platform%22%3A%20%22%7B%7D%22%2C%22utm%22%3A%20%22%7B%7D%22%2C%22referrerDomain%22%3A%20%22%22%2C%22zs%22%3A%200%2C%22sc%22%3A%200%2C%22cuid%22%3A%20%22c7ZIxNgogWs%2C%22%7D; Hm_lvt_f0cfcccd7b1393990c78efdeebff3968=1546779794,1547624412,1547810626,1549011433; Hm_lpvt_f0cfcccd7b1393990c78efdeebff3968=1549011433; cvde=5c5409e7bfa3a-2"
+    "Cookie": "imooc_uuid=da540210-bba5-43cd-ae18-20d417a3e000; imooc_isnew_ct=1545119654; zg_did=%7B%22did%22%3A%20%22167c04ee69e0-0b6c173be5e1c-35657600-1fa400-167c04ee69f453%22%7D; imooc_isnew=2; loginstate=1; apsid=EyNGE4ZjdlMWU1ZTA3ZjA4ZDdlNGExMmZjNTkyNDcAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAANDk4Mzc3OQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADExM2FhNWRmNTVjYWI4MjNiMDFkNjFmYTI4ZWFlMGYwB%2BA%2BXAfgPlw%3DOW; IMCDNS=0; zg_f375fe2f71e542a4b890d9a620f9fb32=%7B%22sid%22%3A%201549066532280%2C%22updated%22%3A%201549066532288%2C%22info%22%3A%201549011432367%2C%22superProperty%22%3A%20%22%7B%5C%22%E5%BA%94%E7%94%A8%E5%90%8D%E7%A7%B0%5C%22%3A%20%5C%22%E6%85%95%E8%AF%BE%E7%BD%91%E6%95%B0%E6%8D%AE%E7%BB%9F%E8%AE%A1%5C%22%2C%5C%22Platform%5C%22%3A%20%5C%22web%5C%22%7D%22%2C%22platform%22%3A%20%22%7B%7D%22%2C%22utm%22%3A%20%22%7B%7D%22%2C%22referrerDomain%22%3A%20%22blog.csdn.net%22%2C%22zs%22%3A%200%2C%22sc%22%3A%200%2C%22cuid%22%3A%20%22c7ZIxNgogWs%2C%22%7D; Hm_lvt_f0cfcccd7b1393990c78efdeebff3968=1547624412,1547810626,1549011433,1549066532; Hm_lpvt_f0cfcccd7b1393990c78efdeebff3968=1549066532; cvde=5c54e123859fa-2"
 }
 
 
 /**
  * 解析html页面
- * @param {object} headers request headers
- * @param {couseId} every course has id 
- * @returns {undefined} undefined
+ * @param {Object} headers request headers
+ * @param {Number} courseId every course has id 
+ * @returns undefined
  */
 
 const superagentHtml = (headers = {}, couseId = 441) => {
@@ -40,9 +40,7 @@ const superagentHtml = (headers = {}, couseId = 441) => {
 
         
         //如果请求失败 
-        if (err) {
-            throw new Error(err)
-        }
+        if (err) throw err
 
         //成功请求到页面后
         const { text } = res
@@ -54,8 +52,13 @@ const superagentHtml = (headers = {}, couseId = 441) => {
 
         //获取所有视频的id和name
         let videoDoms = document.querySelectorAll('.chapter')
+        let imgDoms = document.getElementsByTagName('img')
 
         videoDoms = Array.from(videoDoms)
+        imgDoms = Array.from(imgDoms)
+
+        //下载图片
+        downloadImages(imgDoms)
         
         const videos = []
         
@@ -80,13 +83,10 @@ const superagentHtml = (headers = {}, couseId = 441) => {
             videoInfo.ids = ids
             videoInfo.filename = filename
 
-            getVideoUrl(ids)
+            getVideoUrl(ids, filename)
 
             videos.push(videoInfo)
-
         })
-
-
     })
 }
 
@@ -98,67 +98,129 @@ const superagentHtml = (headers = {}, couseId = 441) => {
  * @returns {Array} 视频下载地址
  */
 
-const getVideoUrl = (ids = []) => {
+const getVideoUrl = (ids = [], filename) => {
 
     if (ids && ids.length > 0) {
-
-        const idUrls = []
         
         ids.forEach((id, index) => {
 
             const videoUrl = `http://www.imooc.com/course/ajaxmediainfo/?mid=${id}&mode=flash`
 
+            //请求下载视频地址
             superagent.get(videoUrl).end((err, res) => {
-                        
-                    if(err) {
-                        throw new Error(err)
-                    }
-        
-                    const { mpath } = JSON.parse(res.text).data.result
 
+                if (err) throw err
 
-                    mpath.forEach((path, index) => {
-                        downloadVideo(path, index)
-                    })
+                const parsedRes = JSON.parse(res.text)
 
-                    })
+                const { result } = parsedRes
+
+                if (result === 0) {
+                    
+                    const mpath = parsedRes.data.result.mpath[0]
+
+                    //下载视频
+                    filename += index
+                    downloadVideo(mpath, filename)
+
+                }
             })
+        })
     }
 }
 
 /**
+ * 下载图片
+ * @param {Array} imgDoms
+ */
+
+ const downloadImages = imgDoms => {
+     if (imgDoms && imgDoms.length > 0) {
+         console.log(imgDoms)
+         
+        const loadImg = (url,filename) => {
+            
+            if (typeof url !== 'string' && typeof filename !== 'string') {
+                
+                throw TypeError('arguments must be a string')
+            }
+
+            const writeStream = fs.createWriteStream(`./assets/${filename}.jpg`, {})
+
+            writeStream.on('close', () => {
+                console.log(`img ${filename} load over`)
+            })
+
+            const req = superagent.get(url)
+            req.pipe(writeStream)
+        }
+
+        imgDoms.forEach((img, index) => {
+            if (img) {
+                
+                const imgSrc = 'http:' + img.getAttribute('src')
+                const filename = img.getAttribute('alt') ? img.getAttribute('alt') : `img${index}`
+
+                fs.exists(path.resolve(__dirname, 'assets/'), isExists => {
+                    isExists ? loadImg(imgSrc, filename)
+                             : fs.mkdir(path.resolve(__dirname, 'assets/'), { recursive : true}, err => {
+                                 
+                                if (err) throw err
+
+                                loadImg(imgSrc, filename)
+                             })
+                            
+                })
+            }
+        })
+     }
+ }
+
+/**
  * 下载视频
- * @param {String} url
+ * @param {String} url 视频链接
+ * @param {String} filename 视频名称
  */
 
 const downloadVideo = (url, filename) => {
     
-    console.log(url)
-
-    if (!url) {
-        return
+    if (typeof url !== 'string' || typeof filename !== 'string') {
+        
+        throw new TypeError('the type of arguments must be a string ')
     }
 
-    const savePath = path.resolve(__dirname, 'videos')
+    const savePath = path.resolve(__dirname, 'videos/')
 
-    const writeStream = fs.createWriteStream(`${savePath}${String(filename)}.mp4`)
+    //开始下载
+    const startDownLoad = () => {
+        
+        const writeStream = fs.createWriteStream(`./videos/${filename}.mp4`, {})
 
-    writeStream.on('close', () => {
-        console.log('download over')
-    })
+        writeStream.on('close', () => {
+            console.log('download over')
+        })
 
+        console.log(url)
+
+        const req = superagent.get('blob:https://www.bilibili.com/4419dc62-15dc-4303-9e8f-11228e690b3c')
+
+        req.pipe(writeStream)
+    }
+
+    //判断存储视频的目录是否存在
     fs.exists(savePath, isExists => {
         if (isExists) {
-
-            const req = superagent.get(url)
-            req.pipe(writeStream)
+            
+            startDownLoad()
 
         }else {
+            
             fs.mkdir(savePath, { recursive: true }, err => {
-                
-                const req = superagent.get(url)
-                req.pipe(writeStream)
 
+                if (err) throw err
+
+                startDownLoad()
+                
             })
         }
     })
